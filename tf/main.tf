79cb48175a92da2e2de4036a5482ab3ab3c46b5c
luminosita <milosh@emisia.net>
2024-10-17 21:41:12 +0200

    minor

--- tf/main.tf
+++ tf/main.tf
@@ -1,20 +1,33 @@
-locals {
+module "cloudinit" {
+  source  = "../../terraform-proxmox-cloudinit" #"luminosita/cloudinit/proxmox"
+  #version = "0.0.3"
+
+  providers = {
+    proxmox = proxmox
+  }
+
+  os = { 
+    vm_base_url                 = "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
+    vm_base_image               = "noble-server-cloudimg-amd64.img"
+    vm_base_image_checksum      = "fad101d50b06b26590cf30542349f9e9d3041ad7929e3bc3531c81ec27f2c788"    
+    vm_base_image_checksum_alg  = "sha256"
+    vm_node_name                = "proxmox"
+  }
+
   images = {
     "ubuntu-noble" = {
-      vm_user                     = "ubuntu"
-      vm_ssh_public_key_files     = [
-        "~/.ssh/id_rsa.pub",
-        "~/.ssh/id_rsa.proxmox.pub"
-      ]
-      
       vm_id                       = 1001
       vm_name                     = "ubuntu-noble"
       vm_node_name                = "proxmox"
 
       vm_cloud_init               = true
 
-      vm_ci_reboot_enabled        = true
-
+      vm_ci_user                     = "ubuntu"
+      vm_ci_ssh_public_key_files     = [
+        "~/.ssh/id_rsa.pub",
+        "~/.ssh/id_rsa.proxmox.pub"
+      ]
+      
       vm_ci_run_cmds      = {
         enabled = true
         content = [
@@ -34,24 +47,5 @@ locals {
       }
     }
   }
-}
-
-module "cloudinit" {
-  source  = "../../terraform-proxmox-cloudinit" #"luminosita/cloudinit/proxmox"
-  #version = "0.0.3"
-
-  providers = {
-    proxmox = proxmox
-  }
-
-  os = { 
-    vm_base_url                 = "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
-    vm_base_image               = "noble-server-cloudimg-amd64.img"
-    vm_base_image_checksum      = "fad101d50b06b26590cf30542349f9e9d3041ad7929e3bc3531c81ec27f2c788"    
-    vm_base_image_checksum_alg  = "sha256"
-    vm_node_name                = "proxmox"
-  }
-
-  images = local.images
 }
 
